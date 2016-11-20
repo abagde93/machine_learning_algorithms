7d165d0fa00cf02acbe71b51edc88c41b460a877
Ajinkya Bagde <ajinkya.abhijit.bagde@gmail.com>
2016-11-20 07:07:57 +0000

    Added functions for image manipulation for modularity
    Come up with a better way to name images ASAP --> commented out code for os.path batch processing in code, find clean way to use it in existing functions

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -6,17 +6,22 @@ from PIL import Image
 from scipy import misc
 import glob
 
-#To save images
-import webbrowser
+#To deal with filepath/filename manipulation, will be useful later
+import os.path
 
-#Create array to store individual image arrays in 
+#Create array to store individual image matrixes in 
 image_array = []
 
-#Convert individual images to arrays and store in image_array
-for image_path in glob.glob("/home/pi/machine_learning_algorithms/Sample011/*.png"):
+
+def image2Array(image):
    
+    #Get name of image from path
+    #name = os.path.basename(image)
+    #name_array.append(name)
+    #print(name)
+    
     #Open image as greyscale
-    img = PIL.Image.open(image_path).convert("L")
+    img = PIL.Image.open(image).convert("L")
     print(img.size)
 
     #Convert image to numpy array. 
@@ -27,13 +32,31 @@ for image_path in glob.glob("/home/pi/machine_learning_algorithms/Sample011/*.pn
     image_array.append(arr)
     print("images in array: %s" % len(image_array))
 
-#Take first matrix from image_array, to make sure image is correct
-back_to_image = PIL.Image.fromarray(image_array[1])
-back_to_image.format = "PNG"
 
-filename = "test_pic.png"
-back_to_image.save(filename)
-webbrowser.open('back_to_image')
+def reconstructImage(matrix_image,i):
+    back_to_image = PIL.Image.fromarray(matrix_image)
+    back_to_image.format = "PNG"
+
+    filename = "%d.png" % i
+    print(filename)
+    back_to_image.save(filename)
+
+def main():
+    for image_path in glob.glob("/home/pi/machine_learning_algorithms/Sample011/*.png"):
+        
+        #Convert image to matrix, and add to image_array
+        image2Array(image_path)
+    
+    #########################################################################################
+    # Make indivdual functions for each classifier and use image2Array and reconstructImage #
+    # This is simply a proof of concept for a possible modular structure                    #
+    #########################################################################################
 
 
 
+    for i in range(0,len(image_array)):
+        #Convert individual matrixes in image_array back to images, and save accordingly
+        reconstructImage(image_array[i],i)
+
+if __name__ == '__main__':
+    main()
